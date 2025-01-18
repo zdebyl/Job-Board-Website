a0a2f086dcd175010ac83152bf901cf1592d139e
Zoe <zoedebyl@gmail.com>
2025-01-18 17:31:10 -0500

    add jobs list

--- resources/views/about.blade.php
+++
@@ -1,6 +0,0 @@
-<x-layout>
-    <x-slot:heading>
-        About Page
-    </x-slot:heading>
-    <h1>hello from the about page</h1>
-</x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,5 @@
     <x-slot:heading>
         Home Page
     </x-slot:heading>
-    <h1>{{$greeting}} from the home page</h1>
+
 </x-layout>
