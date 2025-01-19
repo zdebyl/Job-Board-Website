fa035c626b9cae1275d083b33e1a6a313822e9fb
Zoe <zoedebyl@gmail.com>
2025-01-18 22:07:49 -0500

    add factories and seeders

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\Paginator;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::preventLazyLoading();
+
+        // Paginator::useBootstrapFive();
     }
 }
